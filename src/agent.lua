7e12b99556f3bed61b3ec945e81df64b339f0c3b
Daniel Seybold <daniel_seybold@hotmail.com>
2020-04-21 11:41:04 -0500

    Added getLinedUp and getBlockedScore

--- src/agent.lua
+++ src/agent.lua
@@ -91,10 +91,85 @@ end
   @param board  Board instance
   @param color  Color to check for a line
   @return between 0 and 4 based on number of pieces in a line
+  @return point if line is of length 3, this returns the point that is missing
+  @return direction of line if line has length of 3
 ]]
 local function getLinedUp(board, color)
-  -- TODO
-  return 0
+  -- Holds current highest number of pawns in a line
+  local linedUp = 0
+  -- holds missing point from a line of 3
+  local missingPoint = nil
+  -- holds direction of line (1: horizontal, 2: vertical, 3: positive diagonal, 4: negative diagonal)
+  local lineDir = 0
+  local maxCoord = board:getSize() - 1
+  local directions = {Point(1, 0), Point(0, 1)}
+
+  -- Loop through horizontal vs. vertical
+  for i = 1,2 do
+    -- Loop through rows or columns
+    for sIndex = 0, maxCoord do
+      -- Holds current number of pawns in line
+      local check = 0
+      -- Starting point of the line being checked
+      local start = directions[3-i] * sIndex
+      -- Used to save any missing points from a line
+      local tempMissingPoint
+      -- Loop through each point in the line
+      for oIndex = 0, maxCoord do
+        -- Find point based off of offset
+        local point = start + directions[i] * oIndex
+        -- Check if board has proper color at point in question
+        if board:isColorAt(color, point) then
+            check = check + 1
+        else
+            -- If not correct color, save point in case it is a line of three
+            tempMissingPoint = point
+        end
+      end
+      -- If current line has more than current max, replace it
+      if check > linedUp then
+          linedUp = check
+      end
+      -- If line has 3, then save the point if needed for blocked score
+      if check == 3 then
+          missingPoint = tempMissingPoint
+          -- For loop checks horizontal and then vertical, save index for line Direction
+          lineDir = i
+      end
+    end
+  end
+
+  local diagonalStarts = {Point(0,0), Point(0, maxCoord)}
+  local offset = {Point(1,1), Point(1,-1)}
+  for i = 1, 2 do
+    -- Holds current number of pawns in line
+    local check = 0
+    -- Starting point of the line being checked
+    local tempMissingPoint
+    -- Loop through each point in the line
+    for oIndex = 0, maxCoord do
+      local point = diagonalStarts[i] + offset[i] * oIndex
+      -- Check if board has proper color at point in question
+      if board:isColorAt(color, point) then
+          check = check + 1
+      else
+          -- If not correct color, save point in case it is a line of three
+          tempMissingPoint = point
+      end
+    end
+    -- If current line has more than current max, replace it
+    if check > linedUp then
+        linedUp = check
+    end
+    -- If line has 3, then save the point if needed for blocked score
+    if check == 3 then
+        missingPoint = tempMissingPoint
+        -- For loop checks positive diagonal then negative diagonal
+        -- 2 is to offset for horizontal and vertical
+        lineDir = 2 + i
+    end
+  end
+  return linedUp, missingPoint, lineDir
 end
 
 --[[--
@@ -102,17 +177,43 @@ end
 
   @param board  Board instance
   @param color  Color being blocked
+  @return score 0, 1, or 2 based on the board
 ]]
 local function getBlockedScore(board, color)
-  -- TODO
-  -- 0 if no lines of 3
   -- find our pieces, getLinedUp(board, color)
-  -- count opponents pieces in line
-    -- if it cannot be captured, worth 2
-      -- if not piece:getValidMoves():contains(blocker:getSpace())
-    -- if it can be captured by a piece in the line, worth 1
-    -- if it can be captured by a piece not in the line, worth 0
-  return 0
+  local linedUp, point, dir = getLinedUp(board, color)
+  -- max score is two and gets reduced as better options are found
+  local score = 2
+  -- 0 if no lines of 3
+  if linedUp < 3 then
+    return 0
+  end
+  -- 0 if no pawn at the point
+  if not board:isPawnAt(point) then
+    return 0
+  end
+  for i = 1, board:getPawnCount(color) do
+    local pawn = board:getPawn(color, i)
+    local tempScore = 0
+    if pawn:getValidMoves():contains(point) then
+      local pawnPosition = pawn:getSpace()
+      if (dir == 1 and pawnPosition.x == point.x) or
+         (dir == 2 and pawnPosition.y == point.y) or
+         (dir == 3 and pawnPosition.x == pawnPosition.y) or
+         (dir == 4 and pawnPosition.x == (3 - pawnPosition.y)) then
+           -- if it can be captured by a piece in the line, worth 1
+        tempScore = 1
+      else
+        -- if it can be captured by a piece not in the line, worth 0
+        tempScore = 0
+      end
+    else
+      -- if it cannot be captured, worth 2
+      tempScore = 2
+    end
+    score = math.min(score, tempScore)
+  end
+  return score
 end
 
 --[[--
