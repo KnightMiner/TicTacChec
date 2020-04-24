423b80579e8ab81674a01b677d632074cd1a9815
KnightMiner <knightminer4@gmail.com>
2020-04-24 15:02:08 -0500

    Run pieces in lua debug in the same order as generation

--- src/luaDebug.lua
+++ src/luaDebug.lua
@@ -16,31 +16,39 @@ board = Board(4)
 -- or use require to set a specific type
 local PawnType = require("pawnTypes/pawn")
 local types = {
-  rook   = true,
-  knight = true,
-  bishop = true,
-  pawn   = true
+  "rook",
+  "pawn",
+  "bishop",
+  "knight",
 }
 
 -- objects so we can interact with pieces
 white = {}
 black = {}
 
+function printTable(data)
+  print(table.concat(data, ", "))
+end
+
 -- add all pawns to the board
 local count = 0
-for key, type in pairs(types) do
+for key, pawnType in pairs(types) do
   count = count + 1
-  if type == true then
-    type = require("pawnTypes/" .. key)
+  local icon
+  if type(pawnType) == "string" then
+    icon = pawnType
+    pawnType = require("pawnTypes/" .. pawnType)
+  else
+    icon = pawnType:GetIcon()
   end
-  white[key] = Pawn(type, Color.WHITE)
-  black[key] = Pawn(type, Color.BLACK)
-  board:addPawn(white[key])
-  board:addPawn(black[key])
+  white[icon] = Pawn(pawnType, Color.WHITE)
+  black[icon] = Pawn(pawnType, Color.BLACK)
+  board:addPawn(white[icon])
+  board:addPawn(black[icon])
 end
 
 -- set up network stuff
-def = Agent.makeDefinition{players = 2, pawns = count, layers = {6, 6, 7}}
+def = Agent.makeDefinition{players = 2, pawns = count, layers = {7}}
 white.agent = Agent{network = def:generate()}
 white.agent:setBoard(board, Color.WHITE)
 black.agent = Agent{network = def:generate()}
@@ -50,7 +58,7 @@ gens[1] = Generation.generate(def, 100)
 gens[1]:playGames(5, 20)
 print(string.format("Generation 1, best %.1f, average %.2f", gens[1]:getBestAgent():getAverageScore(), gens[1]:getAverageScore()))
 for i = 2, 20 do
-  gens[i] = gens[i-1]:reproduce(100, 1 / i)
+  gens[i] = gens[i-1]:reproduce(100, 1 / i, 10)
   gens[i]:playGames(5, 10 * math.floor(math.sqrt(i)))
   print(string.format("Generation %d, best %.1f, average %.2f", i, gens[i]:getBestAgent():getAverageScore(), gens[i]:getAverageScore()))
 end
