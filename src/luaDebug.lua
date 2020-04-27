9625417997d26c0463fe196ef87297fbf769c36e
KnightMiner <knightminer4@gmail.com>
2020-04-27 17:27:46 -0500

    Improve luadebug for tests we will run

--- src/luaDebug.lua
+++ src/luaDebug.lua
@@ -19,10 +19,16 @@ local pieces = {
 -- objects so we can interact with pieces
 board = nil
 
+--[[--
+  Quick function to print an array as a comma separated string
+]]
 function printTable(data)
   print(table.concat(data, ", "))
 end
 
+--[[--
+  Sets up a game to play against an agent
+]]
 function prepareGame(agent)
   _G.agent = agent
   board = agent:makeBoard(Color.BLACK)
@@ -35,19 +41,35 @@ function prepareGame(agent)
   end
 end
 
--- set up network stuff
-def = Agent.makeDefinition{players = 2, pawns = 4, layers = {7, 7}}
-local gen = Generation.generate(def, 100)
-gens = gen:run{
-  generations = 10,
-  -- breeding
-  mutationChance = 0.05,
-  clones = 8,
-  rand = 2,
-  -- games
-  games = 5,
-  moves = 20,
-  frequency = 3
-}
+--[[
+Generations:
+  Gen 0: control
+  Gen 1: mutationChance = 0.1
+  Gen 2: layers = {32, 24}
+  Gen 3: frequency = 2
+  Gen 4: pawns = 5
+  Gen 5: layers = {8, 6}
+]]
+--[[--
+  Runs the geneations as set up
+]]
+function run()
+  local def = Agent.makeDefinition{players = 2, pawns = 4, layers = {16, 12}}
+  local time = os.time()
+  print("Starting generations")
+  local gen = Generation.generate(def, 100)
+  gens = gen:run{
+    generations = 250,
+    -- breeding
+    mutationChance = 0.05,
+    clones = 8,
+    rand = 2,
+    -- games
+    games = 5,
+    moves = 50,
+    frequency = 5
+  }
+  print(string.format("Finished generations in %d seconds", os.time() - time))
+end
 
 return ""
