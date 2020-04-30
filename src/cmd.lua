cbb19d0d96e84c127265cdbe6536d55cec894af9
KnightMiner <knightminer4@gmail.com>
2020-04-30 15:29:38 -0500

    Add support for playing against an agent in cmd.lua
    Can also specify starting team and number of pieces

--- src/cmd.lua
+++ src/cmd.lua
@@ -4,18 +4,46 @@
 local Color = require("color")
 local Board = require("board")
 local Point = require("point")
+local Generation = require("generation")
+
+-- fetch args
+local args = {...}
+assert(#args < 3, "Usage: lua cmd.lua [color] [generation]")
+local agent
+-- first arg determines starting player
+local isWhite = true
+if #args > 0 then
+  assert(args[1] == "black" or args[1] == "white", "Invalid color")
+  isWhite = args[1] ~= "black"
+end
+-- second arg is either count or agent
+local count = 4
+if #args == 2 then
+  -- numbers are count
+  local number = tonumber(args[2])
+  if number ~= nil then
+    count = number
+  else
+    -- anything else is agent
+    local gen = Generation.read(args[2])
+    agent = gen:getBestAgent()
+    count = agent:getPawnCount()
+  end
+  assert(count > 2 and count <= 5, "Invalid count, must be between 3 and 5")
+end
 
 -- create board
-local board = Board(4)
+local board = Board(count)
 
 -- add pieces to the board
 -- true means use the key name as the pawn type
 -- or use require to set a specific type
 local types = {
-  rook   = true,
-  knight = true,
-  bishop = true,
-  pawn   = true
+  "rook",
+  "pawn",
+  "bishop",
+  "knight",
+  "king"
 }
 
 -- piece short names
@@ -23,7 +51,8 @@ local aliases = {
   kn = "knight",
   ro = "rook",
   bi = "bishop",
-  pa = "pawn"
+  pa = "pawn",
+  ki = "king"
 }
 
 -- objects so we can interact with pieces
@@ -34,54 +63,64 @@ print("Valid pieces:")
 
 -- add all pawns to the board
 local Pawn = require("pawn")
-for key, type in pairs(types) do
-  print("* " .. key)
-  if type == true then
-    type = require("pawnTypes/" .. key)
-  end
+for i = 1, count do
+  local type = types[i]
+  print("* " .. type)
+  local class = require("pawnTypes/" .. type)
   --- create pieces
-  white[key] = Pawn(type, Color.WHITE)
-  black[key] = Pawn(type, Color.BLACK)
+  white[type] = Pawn(class, Color.WHITE)
+  black[type] = Pawn(class, Color.BLACK)
   -- add to game
-  board:addPawn(white[key])
-  board:addPawn(black[key])
+  board:addPawn(white[type])
+  board:addPawn(black[type])
+end
+
+-- setup agent
+if agent then
+  agent:setBoard(board, Color.BLACK)
 end
 
 -- alternate players
-local isWhite = true
 while board:getWinner() == nil do
   -- information
   print()
   local color = isWhite and Color.WHITE or Color.BLACK
   print(Color.color(color, color:getName() .. "'s turn"))
   print(board)
-  -- prompt
-  local input = io.read()
-  if input == "exit" then break end
-  local match = {string.gmatch(input, "(%w+) (%d+),%s*(%d+)")()}
-  -- insure data is valid
-  if #match == 3 then
-    -- ensure piece is valid
-    local team = isWhite and white or black
-    local name = match[1]:lower()
-    name = aliases[name] or name
-    local pawn = team[name]
-    if pawn == nil then
-      print("Invalid piece " .. name)
-    else
-      -- ensure move is valid
-      local validMoves = pawn:getValidMoves()
-      local point = Point(tonumber(match[2]), tonumber(match[3]))
-      if validMoves:contains(point) then
-        pawn:moveOrAddTo(point)
-        isWhite = not isWhite
-      else
-        print("Invalid move for " .. name .. ", valid moves:")
-        print(validMoves)
-      end
-    end
+
+  --- agent move making
+  if not isWhite and agent then
+    agent:makeMove()
+    isWhite = true
   else
-    print("Invalid input, format: 'piece x,y'")
+    -- prompt
+    local input = io.read()
+    if input == "exit" then break end
+    local match = {string.gmatch(input, "(%w+) (%d+),%s*(%d+)")()}
+    -- insure data is valid
+    if #match == 3 then
+      -- ensure piece is valid
+      local team = isWhite and white or black
+      local name = match[1]:lower()
+      name = aliases[name] or name
+      local pawn = team[name]
+      if pawn == nil then
+        print("Invalid piece " .. name)
+      else
+        -- ensure move is valid
+        local validMoves = pawn:getValidMoves()
+        local point = Point(tonumber(match[2]), tonumber(match[3]))
+        if validMoves:contains(point) then
+          pawn:moveOrAddTo(point)
+          isWhite = not isWhite
+        else
+          print("Invalid move for " .. name .. ", valid moves:")
+          print(validMoves)
+        end
+      end
+    else
+      print("Invalid input, format: 'piece x,y'")
+    end
   end
 end
 
